0d079fbc32b50a5217fef070d73942b0f9a3f402
AgusRey04 <reymundez37@gmail.com>
2025-09-18 16:14:28 -0300

    modified:   AlquilerMaq/package-lock.json 	modified:   AlquilerMaq/package.json 	modified:   AlquilerMaq/src/App.jsx 	modified:   AlquilerMaq/src/components/Newproduct/NewProduct.jsx 	modified:   AlquilerMaq/src/components/auth/login/Login.jsx 	new file:   AlquilerMaq/src/components/dashboar/AdminPanel.jsx 	modified:   AlquilerMaq/src/components/dashboar/Mainscren.jsx 	new file:   AlquilerMaq/src/components/dashboar/NotFound.jsx

--- AlquilerMaq/src/App.jsx
+++ AlquilerMaq/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import FormLogin from './components/auth/FormLogin'
-import FormRegister from './components/auth/FormRegister';
-import MainScreen from './components/dashboard/MainSceen'
-import NotFound from './components/dashboard/NotFound';
-import AdminPanel from './components/dashboard/AdminPanel';
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/auth/login/Login";
 
+import MainScreen from "./components/dashboar/Mainscren";
+import NotFound from "./components/dashboar/NotFound";
+import AdminPanel from "./components/dashboar/AdminPanel";
 
 function App() {
   const ProtectedRoute = ({ isSignedIn }) => {
@@ -16,40 +20,50 @@ function App() {
       return <Navigate to="/login" replace />;
     }
     return <Outlet />;
-  }
+  };
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
 
   const handleLogIn = () => {
     setLoggedIn(true);
-  }
+  };
   const handleLogOut = () => {
     setLoggedIn(false);
     setUser(null);
-  }
-  
+  };
 
   return (
     <div className="App">
       <BrowserRouter>
-
-      <Routes>
-  <Route path="/" element={<Navigate to="/login" />} />
-  <Route path="/login" element={<FormLogin onLogin={handleLogIn} setUser={setUser} />} />
-  <Route path="/register" element={<FormRegister />} />
-  <Route element={<ProtectedRoute isSignedIn={loggedIn} />}>
-    <Route path="/main" element={<MainScreen user={user} setUser={setUser} logOut={handleLogOut} />} />
-    {user?.role === "sysadmin" && (
-    <Route path="/panel-de-control" element={<AdminPanel user={user} />} />
-    )}
-  </Route>
-  <Route path="*" element={<NotFound />} />
-</Routes>
-
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route
+            path="/login"
+            element={<Login onLogin={handleLogIn} setUser={setUser} />}
+          />
+          <Route element={<ProtectedRoute isSignedIn={loggedIn} />}>
+            <Route
+              path="/main"
+              element={
+                <MainScreen
+                  user={user}
+                  setUser={setUser}
+                  logOut={handleLogOut}
+                />
+              }
+            />
+            {user?.role === "sysadmin" && (
+              <Route
+                path="/panel-de-control"
+                element={<AdminPanel user={user} />}
+              />
+            )}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
-
+export default App;
---
+++ AlquilerMaq/src/components/dashboar/AdminPanel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserManagement from "../UserManagement/UserManagement";
+
+const AdminPanel = ({ user }) => {
+  return (
+    <div className="container mt-4">
+      <h2>Panel de Control del SysAdmin</h2>
+      <p>Bienvenido, {user?.username}</p>
+      <UserManagement />
+    </div>
+  );
+};
+
+export default AdminPanel;
