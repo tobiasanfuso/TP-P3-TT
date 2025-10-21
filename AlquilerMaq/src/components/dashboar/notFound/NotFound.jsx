368d382c430b962c8b7a96271d54feb0640a8900
AgusRey04 <reymundez37@gmail.com>
2025-10-21 13:08:25 -0300

    validaciondes front Login - UserRegister

--- AlquilerMaq/src/components/dashboar/notFound/NotFound.jsx
+++ AlquilerMaq/src/components/dashboar/notFound/NotFound.jsx
@@ -4,15 +4,15 @@ import { Button } from "react-bootstrap";
 
 const NotFound = () => {
   const navigate = useNavigate();
-
   const goBackLoginHandler = () => {
     navigate("/login");
   };
+
   return (
     <div className="NotFound vh-100 vw-100 d-flex flex-column justify-content-center align-items-center">
       <h2>Parece que estas perdido...</h2>
       <Button className="text-center button" onClick={goBackLoginHandler}>
-        Volver a iniciar sesion
+        Volver al inicio
       </Button>
     </div>
   );
